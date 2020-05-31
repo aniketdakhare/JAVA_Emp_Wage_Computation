1210a586cef132549f7f107966f408b6ce6544fb
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-31 20:03:49 +0530

    Part time wage calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,26 +5,28 @@ public class EmployeeWage
 	static final int WAGE_PER_HOUR=20;
 	static int daily_hours=8;
 	static int perDayWage;
-	void attendance()
-	{
-		Random r = new Random();
-		int check = r.nextInt(2);
-		if (check == 1)
-		{
-			daily_hours=8;
-		}
-		else
-		{
-			System.out.println("Employee is Absent");
-			daily_hours=0;
-		}
-		perDayWage=daily_hours*WAGE_PER_HOUR;
-		System.out.println("Per day salary of an employee is Rs. "+perDayWage);
-	}
+	static final int FULLTIME=1;
+	static final int PARTTIME=2;
+
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
-		EmployeeWage emp=new EmployeeWage();
-		emp.attendance();
+		Random r = new Random();
+                int check = r.nextInt(3);
+                if (check == FULLTIME)
+                {
+                        daily_hours=8;
+                }
+		else if (check == PARTTIME)
+		{
+			daily_hours=4;
+		}
+                else
+                {
+                        System.out.println("Employee is Absent");
+                        daily_hours=0;
+                }
+                perDayWage=daily_hours*WAGE_PER_HOUR;
+                System.out.println("Per day salary of an employee is Rs. "+perDayWage);
 	}
 }
