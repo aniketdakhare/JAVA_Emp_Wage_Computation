e242b4cd32cacef41e03a420fd5d3e588d4b7020
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-04 20:55:43 +0530

    Monthly Employee wage for multiple company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,39 +2,38 @@ import java.util.Random;
 
 public class EmployeeWage
 {
-	static final int FULLTIME=1;
-	static final int PARTTIME=2;
-	static final int WAGE_PER_HOUR=20;
-	static final int WORKING_DAYS=20;
-	static final int MAX_HOURS=100;
-	void salary()
-	{
-		int daily_hours=0, totalEmpHours=0, totalWorkingDays=0;
-		Random r = new Random();
-		while (totalEmpHours < MAX_HOURS && totalWorkingDays < WORKING_DAYS )
-		{
-			totalWorkingDays++;
-			int check = r.nextInt(3);
-			switch (check)
-			{
-				case FULLTIME:
-					daily_hours=8;
-					break;
-				case PARTTIME:
-					daily_hours=4;
-					break;
-				default:
-					daily_hours=0;
-			}
-			totalEmpHours+=daily_hours;
-		}
-		int empWageperMonth=totalEmpHours*WAGE_PER_HOUR;
-		System.out.println("Monthly salary of an employee is Rs. "+empWageperMonth);
-	}
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome to Employee Wage Computation Program");
-		EmployeeWage emp=new EmployeeWage();
-		emp.salary();
-	}
+        static final int FULLTIME=1;
+        static final int PARTTIME=2;
+
+        void salary(String company, int wage_per_hour, int working_days, int max_hour_per_month)
+        {
+                int daily_hours=0, totalEmpHours=0, totalWorkingDays=0;
+                Random r = new Random();
+                while (totalEmpHours < max_hour_per_month && totalWorkingDays < working_days )
+                {
+                        totalWorkingDays++;
+                        int check = r.nextInt(3);
+                        switch (check)
+                        {
+                                case FULLTIME:
+                                        daily_hours=8;
+                                        break;
+                                case PARTTIME:
+                                        daily_hours=4;
+                                        break;
+                                default:
+                                        daily_hours=0;
+                        }
+                        totalEmpHours+=daily_hours;
+                }
+                int empWageperMonth=totalEmpHours*wage_per_hour;
+                System.out.println("Monthly Employee wage of "+company+" is Rs. "+empWageperMonth);
+        }
+        public static void main(String[] args)
+        {
+                System.out.println("Welcome to Employee Wage Computation Program");
+                EmployeeWage emp=new EmployeeWage();
+                emp.salary("Dmart",20,22,104);
+                emp.salary("Relience Fresh",25,20,96);
+        }
 }
