085840461e4e5d591255c6481dafee78d2273431
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-04 21:26:03 +0530

    Saved the total wage for each company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,38 +2,54 @@ import java.util.Random;
 
 public class EmployeeWage
 {
-        static final int FULLTIME=1;
-        static final int PARTTIME=2;
+	public static final int FULLTIME=1;
+	public static final int PARTTIME=2;
 
-        void salary(String company, int wage_per_hour, int working_days, int max_hour_per_month)
-        {
-                int daily_hours=0, totalEmpHours=0, totalWorkingDays=0;
-                Random r = new Random();
-                while (totalEmpHours < max_hour_per_month && totalWorkingDays < working_days )
-                {
-                        totalWorkingDays++;
-                        int check = r.nextInt(3);
-                        switch (check)
-                        {
-                                case FULLTIME:
-                                        daily_hours=8;
-                                        break;
-                                case PARTTIME:
-                                        daily_hours=4;
-                                        break;
-                                default:
-                                        daily_hours=0;
-                        }
-                        totalEmpHours+=daily_hours;
-                }
-                int empWageperMonth=totalEmpHours*wage_per_hour;
-                System.out.println("Monthly Employee wage of "+company+" is Rs. "+empWageperMonth);
-        }
-        public static void main(String[] args)
-        {
-                System.out.println("Welcome to Employee Wage Computation Program");
-                EmployeeWage emp=new EmployeeWage();
-                emp.salary("Dmart",20,22,104);
-                emp.salary("Relience Fresh",25,20,96);
-        }
+	private final int WAGE_PER_HOUR;
+	private final int WORKING_DAYS;
+	private final int MAX_HOURS;
+	private final String COMPANY;
+	private int empWageperMonth;
+
+	public EmployeeWage(String company, int wage_per_hour, int working_days, int max_hour)
+	{
+		this.WAGE_PER_HOUR=wage_per_hour;
+		this.WORKING_DAYS=working_days;
+		this.MAX_HOURS=max_hour;
+		this.COMPANY=company;
+	}
+
+	void salary()
+	{
+		int daily_hours=0, totalEmpHours=0, totalWorkingDays=0;
+		Random r = new Random();
+		while (totalEmpHours < MAX_HOURS && totalWorkingDays < WORKING_DAYS )
+		{
+			totalWorkingDays++;
+			int check = r.nextInt(3);
+			switch (check)
+			{
+				case FULLTIME:
+					daily_hours=8;
+					break;
+				case PARTTIME:
+					daily_hours=4;
+					break;
+				default:
+					daily_hours=0;
+			}
+			totalEmpHours+=daily_hours;
+		}
+		empWageperMonth=totalEmpHours*WAGE_PER_HOUR;
+        	System.out.println("Monthly Employee wage of "+COMPANY+" is Rs. "+empWageperMonth);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Wage Computation Program");
+		EmployeeWage Dmart=new EmployeeWage("Dmart",20,22,104);
+		EmployeeWage Relience=new EmployeeWage("Relience Fresh",25,20,96);
+		Dmart.salary();
+		Relience.salary();
+	}
 }
